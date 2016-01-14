e84b6c63bbd8abc439408938198e455282f1c46c
Christoph Buelter <c.buelter@arcor.de>
2016-01-14 23:10:10 +0100

    Wrap code in closure. Use 2 spaces for indentation. Chain controllers and filte ron app.

--- pacer_app.js
+++ pacer_app.js
@@ -1,85 +1,95 @@
-var app = angular.module("pacer_app", []);
-app.controller("pacer_ctrl", function($scope) {
+(function() {
 
-    $scope.distance = void 0;  // Kilometers.
-    $scope.time = void 0;  // Minutes.
-    $scope.speed = void 0;  // Kilometers per hour.
-    $scope.pace = void 0;  // Minutes per kilometer.
+  angular.module("pacer_app", [])
 
-    $scope.clamp = function(num) {
+    .controller("pacer_date_ctrl", function($scope) {
+      $scope.example = {
+        value: new Date(1970, 0, 0, 0, 0, 0)
+      };
+    });
+
+    .controller("pacer_ctrl", function($scope) {
+      $scope.distance = void 0;  // Kilometers.
+      $scope.time = {hours: void 0, minutes: void 0, seconds: void 0};
+      $scope.speed = void 0;  // Kilometers per hour.
+      $scope.pace = {minutes: void 0, seconds: void 0};
+
+      $scope.clamp = function(num) {
         return Number(num.toFixed(2));
-    }
+      }
 
-    $scope.reset = function() {
+      $scope.reset = function() {
         $scope.distance = void 0;
-        $scope.time = void 0;
-        $scope.pace = void 0;
+        $scope.time = {hours: void 0, minutes: void 0, seconds: void 0};
         $scope.speed = void 0;
-    }
+        $scope.pace = {minutes: void 0, seconds: void 0};
+      }
 
-    $scope.calcPace = function() {
+      $scope.calcPace = function() {
         var pace = $scope.time / $scope.distance;
         return $scope.clamp(pace);
-    }
+      }
 
-    $scope.calcPaceUsingSpeed = function() {
+      $scope.calcPaceUsingSpeed = function() {
         var pace = 60.0 / $scope.speed;
         return $scope.clamp(pace);
-    }
+      }
 
-    $scope.calcSpeed = function() {
+      $scope.calcSpeed = function() {
         var speed = $scope.distance / ($scope.time / 60.0);
         return $scope.clamp(speed);
-    }
+      }
 
-    $scope.calcDistance = function() {
+      $scope.calcDistance = function() {
         var distance = $scope.time / $scope.pace;
         return $scope.clamp(distance);
-    }
+      }
 
-    $scope.calcTime = function() {
+      $scope.calcTime = function() {
         var time = $scope.distance * $scope.pace;
         return $scope.clamp(time);
-    }
+      }
 
-    // Based on the formula here: http://de.wikipedia.org/wiki/Laufsport
-    $scope.estimateTime = function(knownDistance, knownTime, newDistance) {
+      // Based on the formula here: http://de.wikipedia.org/wiki/Laufsport
+      $scope.estimateTime = function(knownDistance, knownTime, newDistance) {
         var factor = newDistance / knownDistance;
         var predictedTime = knownTime * Math.pow(factor, 1.07);
         return $scope.clamp(predictedTime);
-    }
+      }
 
-    $scope.onChangedDistance = function() {
+      $scope.onChangedDistance = function() {
         $scope.pace = $scope.calcPace();
         $scope.speed = $scope.calcSpeed();
-    }
+      }
 
-    $scope.onChangedTime = function() {
+      $scope.onChangedTime = function() {
         $scope.pace = $scope.calcPace();
         $scope.speed = $scope.calcSpeed();
-    }
+      }
 
-    $scope.onChangedPace = function() {
+      $scope.onChangedPace = function() {
         $scope.time = $scope.calcTime();
         $scope.speed = $scope.calcSpeed();
-    }
+      }
 
-    $scope.onChangedSpeed = function() {
+      $scope.onChangedSpeed = function() {
         $scope.pace = $scope.calcPaceUsingSpeed();
         $scope.time = $scope.calcTime();
-    }
-});
+      }
+  })
 
-app.filter("minutesToTimeString", function() {
+  .filter("minutesToTimeString", function() {
     return function(minutes) {
-        var hours = Math.floor(minutes / 60.0);
-        var restMinutes = minutes % 60.0;
-        var seconds = restMinutes * 60.0;
-        var restSeconds = seconds % 60.0;
-        var timeString = "";
-        timeString = hours + "h ";
-        timeString += Math.floor(restMinutes) + "m ";
-        timeString += Math.floor(restSeconds) + "s";
-        return timeString;
+      var hours = Math.floor(minutes / 60.0);
+      var restMinutes = minutes % 60.0;
+      var seconds = restMinutes * 60.0;
+      var restSeconds = seconds % 60.0;
+      var timeString = "";
+      timeString = hours + "h ";
+      timeString += Math.floor(restMinutes) + "m ";
+      timeString += Math.floor(restSeconds) + "s";
+      return timeString;
     }
-});
+  });
+
+})();
